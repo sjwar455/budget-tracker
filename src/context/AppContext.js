499f53c2e9463e64ead83b8e5a86e3f3373384bd
Samantha Wareing <samantha.wareing@gmail.com>
2021-03-13 00:53:23 -0700

    added AppContext context & integrated it with AddExpenseForm, Budget, ExpenseList & App

---
+++ src/context/AppContext.js
@@ -0,0 +1,41 @@
+import React, { useReducer, createContext } from 'react';
+
+const AppReducer = (state, action) => {
+	switch(action.type) { 
+		case 'ADD_EXPENSE':
+			return { 
+				...state,
+				expenses: [...state.expenses, action.payload],
+			};
+		default:
+			return state;
+	}
+};
+
+const initialState = {
+	budget: 4000,
+	expenses: [
+		{ id: 12, name: 'Mortgage', cost: 1600 },
+		{ id: 13, name: 'Utilities', cost: 300 },
+		{ id: 14, name: 'Groceries', cost: 200 },
+	],
+};
+
+export const AppContext = createContext();
+
+export const AppProvider = (props) => { 
+	const [state, dispatch] = useReducer(AppReducer, initialState);
+
+	return(
+		<AppContext.Provider
+			value={{
+				budget: state.budget,
+				expenses: state.expenses,
+				dispatch,
+			}}
+		>
+			{props.children}
+		</AppContext.Provider>
+	);
+
+};
