1165b7cdbb4144e86af637e2e79031e261298d29
Samantha Wareing <samantha.wareing@gmail.com>
2021-03-12 23:31:34 -0700

    added ExpenseList & ExpenseItem

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
-import ExpenseTotal from './components/ExpenseTotal'
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+
 
 const App = () => {
   return (
@@ -19,7 +21,14 @@ const App = () => {
   				<ExpenseTotal />
   			</div>
   		</div>
+  		<h3 className='mt-3'>Expenses</h3>
+  		<div className='row mt-3'>
+  			<div className='col-sm'>
+  				<ExpenseList /> 
+  			</div>
+  		</div>
   	</div>
+
 
   	);
 };
